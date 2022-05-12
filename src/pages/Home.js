603d7c4af9652c7e54567da629e55fb6e25a2cef
Moises De Leon <mdeleon5826@gmail.com>
2022-05-11 23:18:38 -0400

    created sopme styling. finished the searchbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import useFetch from '../useFetch';
 import MultiActionAreaCard from '../components/ImageCard';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
+import { Box } from '@mui/material';
 
 export default function Home() {
   const [ term, setTerm ] = useState('');
@@ -13,20 +14,40 @@ export default function Home() {
   return (
     <>
       <div className="home">
-        <Container sx={{marginY: 5}}>
-          { error && <div>{ error }</div> }
-          { isLoading && <div>Loading...</div> }
-        <form>
+        <h1>Go on, Click me!</h1>
+        { error && console.log(error) }
+        { isLoading && <div>Loading...</div> }
+        <Box>
+          <form action="" className="search-bar">
           <DebounceInput 
-          className='searchbar-input' 
-          type='text' 
-          placeholder="Search..."
+          className='input'
+          type="search" 
+          name="search" 
+          pattern=".*\S.*" required 
+          autoComplete='off'
+          placeholder="What are you looking for?"
           minLength={2}
           debounceTimeout={1000}
           onChange={e => setTerm(e.target.value)}
           ></DebounceInput>
+          <button className="search-btn" type="submit">
+            <span>Search</span>
+          </button>
         </form>
-          <Grid container spacing={4}>
+        </Box>
+        <Container sx={{
+          marginX: 5,
+          margin: 5,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          }}>
+          <Grid container sx={{
+            display: 'grid', 
+            gridTemplateColumns: 'repeat(3, 4fr)',
+            gap: 1,
+            columnGap: 2
+            }}>
             {photos.map((photo, index) => <MultiActionAreaCard photo={photo} key={index}/>)}
           </Grid>
         </Container>
